b2c5af3eaa8ad56408e4db9b6addff72a43edda3
David Wursteisen <david.wursteisen+git@gmail.com>
2020-06-16 22:45:47 +0200

    [PARSER] Unpack texture so it's ready to use in games

--- collada-parser/src/main/kotlin/com/dwursteisen/gltf/parser/material/MaterialParser.kt
+++ collada-parser/src/main/kotlin/com/dwursteisen/gltf/parser/material/MaterialParser.kt
@@ -5,6 +5,7 @@ import com.dwursteisen.gltf.parser.support.isEmissiveTexture
 import com.dwursteisen.minigdx.scene.api.material.Material
 import de.matthiasmann.twl.utils.PNGDecoder
 import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
 
 class MaterialParser(private val gltfAsset: GltfAsset) {
 
@@ -17,10 +18,23 @@ class MaterialParser(private val gltfAsset: GltfAsset) {
                 val data = buffer.buffer.data.copyOfRange(buffer.byteOffset, buffer.byteOffset + buffer.byteLength)
 
                 val decoder = PNGDecoder(ByteArrayInputStream(data))
+                // create a byte buffer big enough to store RGBA values
+                val txt =
+                    ByteBuffer.allocateDirect(4 * decoder.width * decoder.height)
+
+                // decode
+                decoder.decode(txt, decoder.width * 4, PNGDecoder.Format.RGBA)
+
+                // flip the buffer so its ready to read
+                txt.flip()
+
+                val result = ByteArray(txt.remaining())
+                txt.get(result)
+
                 Material(
                     name = m.name ?: "",
                     id = m.index,
-                    data = data,
+                    data = result,
                     width = decoder.width,
                     height = decoder.height
                 )
