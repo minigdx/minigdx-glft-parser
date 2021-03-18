306447385f724613f8091bcd7c2e29fb2f13c0ad
David Wursteisen <david.wursteisen+git@gmail.com>
2021-03-18 08:36:43 +0100

    Bump gradle developer plugin to 1.0.0-alpha4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.github.minigdx.gradle.plugin.developer") version "1.0.0-alpha0"
-    id("com.github.minigdx.gradle.plugin.developer.jvm") version "1.0.0-alpha0" apply false
-    id("com.github.minigdx.gradle.plugin.developer.mpp") version "1.0.0-alpha0" apply false
+    id("com.github.minigdx.gradle.plugin.developer") version "1.0.0-alpha4"
+    id("com.github.minigdx.gradle.plugin.developer.jvm") version "1.0.0-alpha4" apply false
+    id("com.github.minigdx.gradle.plugin.developer.mpp") version "1.0.0-alpha4" apply false
 }
 
 minigdxDeveloper {
